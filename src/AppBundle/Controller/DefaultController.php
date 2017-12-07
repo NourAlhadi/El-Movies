7bc39b2ac13fab2a3aa42ab2429138991f67510d
NourAlhadi <nour.alhadi.99@gmail.com>
2017-12-08 01:16:10 +0200

    finally first admin is done

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,10 +18,12 @@ class DefaultController extends Controller
     {
         $user = $this->getUser();
         if ($user != null && ($user->getId() == 1 || $user->getUsername() == "admin") ){
-            if (!$user->hasRole("ROLE_ADMIN")) $user->addRole('ROLE_ADMIN');
-            $this->getDoctrine()->getManager()->persist($user);
-            $this->getDoctrine()->getManager()->flush();
-            return $this->redirectToRoute('index');
+            if (!$user->hasRole("ROLE_ADMIN")) {
+                $user->addRole('ROLE_ADMIN');
+                $this->getDoctrine()->getManager()->persist($user);
+                $this->getDoctrine()->getManager()->flush();
+                $this->get('fos_user.user_manager')->updateUser($user);
+            }
         }
 
         return $this->render('default/index.html.twig');
