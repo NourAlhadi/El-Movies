079a19f602046d5d54e2ed01ee788d115a06c72d
NourAlhadi <nour.alhadi.99@gmail.com>
2017-12-09 01:54:15 +0200

    Finalizing Movie Details Page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,11 +4,14 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Comment;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use UserBundle\Entity\User;
 
 
@@ -51,7 +54,11 @@ class DefaultController extends Controller
      * @Route("/movie/{id}/",name="movie")
      */
     public function movieAction(Request $request,$id){
-        $movie =$this->getDoctrine()->getManager()->getRepository('AppBundle:Movie')->find($id);
+        $em = $this->getDoctrine()->getManager();
+
+        $movie =$em->getRepository('AppBundle:Movie')->find($id);
+
+
 
         $form = $this->createFormBuilder(null)
             ->add('comment',TextareaType::class)
@@ -59,26 +66,64 @@ class DefaultController extends Controller
 
         $form->handleRequest($request);
 
+        $views = $movie->getViewCount();
+
         if ($form->isSubmitted() && $form->isValid()){
             $arr = $form->getData();
             $c = new Comment();
             $c->setBody($arr['comment']);
             $c->setWriter($this->getUser()->getName());
-            $comments = $this->getDoctrine()->getManager();
-            $comments->persist($c);
-            $comments->flush();
+            $em->persist($c);
+            $em->flush();
 
             $movie->getComments()->add($c);
-            $this->getDoctrine()->getManager()->persist($movie);
-            $this->getDoctrine()->getManager()->flush();
+            $movie->setViewCount($views-1);
+            $em->persist($movie);
+            $em->flush();
 
             return $this->redirectToRoute('movie',["id"=>$id]);
+        }else {
+            $movie->setViewCount($views + 1);
+            $em->persist($movie);
+            $em->flush();
         }
-
         return $this->render('default/movie.html.twig',[
             "movie"=>$movie,
             "form"=>$form->createView(),
         ]);
     }
 
+    /**
+     * @Route("/movie/add/rate",name="add_rate")
+     *
+     * @Method({"GET", "POST"})
+     */
+    public function addRateAction(Request $request){
+        //return $this->render('default/dumpper.html.twig',["dmp"=>$request]);
+        if ($request->isXMLHttpRequest()) {
+            $content = $request->getContent();
+            dump($content);
+            if (!empty($content)) {
+                $params = json_decode($content, true);
+                dump($params);
+                $id = $params['id'];
+                $stars = $params['stars'];
+
+                $em = $this->getDoctrine()->getManager();
+                $movie = $em->getRepository('AppBundle:Movie')->find($id);
+
+                $starcnt = $movie->getStarsCount();
+                $totalcnt = $movie->getTotalCounts();
+
+                $movie->setTotalCounts($totalcnt+1);
+                $movie->setStarsCount($starcnt+$stars);
+
+                $em->persist($movie);
+                $em->flush();
+            }
+            return new JsonResponse(array('data' => $params));
+        }
+
+        return new Response('Error!', 400);
+    }
 }
